57a1e6d0593a46b05087dbcd1209296eb2b9fcdd
tilseb <till@energymodellinglab.com>
2021-09-28 13:54:04 +0200

    Add new data and updated scenarioCombinations.js format

--- src/translations/charts.js
+++ src/translations/charts.js
@@ -5,6 +5,12 @@
         "name_en": "Primary Energy Supply",
         "name_az": "not_defined"
       }
+    ],
+    "chart-titles.tab1.chart2": [
+      {
+        "name_en": "CO2 Emissions",
+        "name_az": "not_defined"
+      }
     ]
   }
 }
--- src/translations/legends.js
+++ src/translations/legends.js
@@ -1,4 +1,4 @@
-export default {
+{
   "legends": {
     "legend0": [
       {
