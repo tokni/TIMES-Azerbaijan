00e5227366cedbf131cca23bca86ee10e036618d
tilseb <till@energymodellinglab.com>
2021-10-19 17:09:42 +0200

    Update translation to Azerbaijani

--- src/translations/legends.js
+++ src/translations/legends.js
@@ -1,662 +1,662 @@
 export default {
   "legend0": {
     "name_en": "Agriculture",
-    "name_az": "Agriculture_AZ"
+    "name_az": "Kənd təsərrüfatı"
   },
   "legend1": {
     "name_en": "Commercial",
-    "name_az": "Commercial_AZ"
+    "name_az": "Ticarət"
   },
   "legend10": {
     "name_en": "Hydrogen",
-    "name_az": "Hydrogen_AZ"
+    "name_az": "Hidrogen"
   },
   "legend100": {
     "name_en": "Wind offshore",
-    "name_az": "Wind offshore_AZ"
+    "name_az": "Külək dənizdə"
   },
   "legend101": {
     "name_en": "Wind onshore",
-    "name_az": "Wind onshore_AZ"
+    "name_az": "Külək quruda"
   },
   "legend102": {
     "name_en": "Waste",
-    "name_az": "Waste_AZ"
+    "name_az": "Tullantı"
   },
   "legend103": {
     "name_en": "Crude oil",
-    "name_az": "Crude oil_AZ"
+    "name_az": "Xam neft"
   },
   "legend104": {
     "name_en": "Cooling",
-    "name_az": "Cooling_AZ"
+    "name_az": "Soyutma"
   },
   "legend105": {
     "name_en": "Cooling",
-    "name_az": "Cooling_AZ"
+    "name_az": "Soyutma"
   },
   "legend106": {
     "name_en": "Cooling",
-    "name_az": "Cooling_AZ"
+    "name_az": "Soyutma"
   },
   "legend107": {
     "name_en": "Electricity",
-    "name_az": "Electricity_AZ"
+    "name_az": "Elektrik enerjisi"
   },
   "legend108": {
     "name_en": "Gasoline",
-    "name_az": "Gasoline_AZ"
+    "name_az": "Benzin"
   },
   "legend109": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend11": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend110": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend111": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend112": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend113": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend114": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend115": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend116": {
     "name_en": "Liquefied petroleum gas",
-    "name_az": "Liquefied petroleum gas_AZ"
+    "name_az": "Mayeləşdirilmiş neft qazı"
   },
   "legend117": {
     "name_en": "Natural gas",
-    "name_az": "Natural gas_AZ"
+    "name_az": "Təbii qaz"
   },
   "legend118": {
     "name_en": "Rapeseed",
-    "name_az": "Rapeseed_AZ"
+    "name_az": "Raps bitkisi"
   },
   "legend119": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend12": {
     "name_en": "Heavy fuel oil",
-    "name_az": "Heavy fuel oil_AZ"
+    "name_az": "Ağır mazut"
   },
   "legend120": {
     "name_en": "Bio Synt. Nat. Gas",
-    "name_az": "Bio Synt. Nat. Gas_AZ"
+    "name_az": "Bio sintetik təbii qaz"
   },
   "legend121": {
     "name_en": "Bio Synt. Nat. Gas",
-    "name_az": "Bio Synt. Nat. Gas_AZ"
+    "name_az": "Bio sintetik təbii qaz"
   },
   "legend122": {
     "name_en": "Solar",
-    "name_az": "Solar_AZ"
+    "name_az": "Günəş"
   },
   "legend123": {
     "name_en": "Straw",
-    "name_az": "Straw_AZ"
+    "name_az": "Saman"
   },
   "legend124": {
     "name_en": "Crude oil",
-    "name_az": "Crude oil_AZ"
+    "name_az": "Xam neft"
   },
   "legend125": {
     "name_en": "Electricity",
-    "name_az": "Electricity_AZ"
+    "name_az": "Elektrik enerjisi"
   },
   "legend126": {
     "name_en": "Natural gas",
-    "name_az": "Natural gas_AZ"
+    "name_az": "Təbii qaz"
   },
   "legend127": {
     "name_en": "Transport",
-    "name_az": "Transport_AZ"
+    "name_az": "Nəqliyyat"
   },
   "legend128": {
     "name_en": "Transport",
-    "name_az": "Transport_AZ"
+    "name_az": "Nəqliyyat"
   },
   "legend129": {
     "name_en": "Transport",
-    "name_az": "Transport_AZ"
+    "name_az": "Nəqliyyat"
   },
   "legend13": {
     "name_en": "Natural gas",
-    "name_az": "Natural gas_AZ"
+    "name_az": "Təbii qaz"
   },
   "legend130": {
     "name_en": "Transport",
-    "name_az": "Transport_AZ"
+    "name_az": "Nəqliyyat"
   },
   "legend131": {
     "name_en": "Diesel blend",
-    "name_az": "Diesel blend_AZ"
+    "name_az": "Dizel qarışığı"
   },
   "legend132": {
     "name_en": "Diesel blend",
-    "name_az": "Diesel blend_AZ"
+    "name_az": "Dizel qarışığı"
   },
   "legend133": {
     "name_en": "Diesel blend",
-    "name_az": "Diesel blend_AZ"
+    "name_az": "Dizel qarışığı"
   },
   "legend134": {
     "name_en": "Diesel blend",
-    "name_az": "Diesel blend_AZ"
+    "name_az": "Dizel qarışığı"
   },
   "legend135": {
     "name_en": "Biodiesel",
-    "name_az": "Biodiesel_AZ"
+    "name_az": "Biodizel"
   },
   "legend136": {
     "name_en": "Diesel",
-    "name_az": "Diesel_AZ"
+    "name_az": "Dizel"
   },
   "legend137": {
     "name_en": "Electricity",
-    "name_az": "Electricity_AZ"
+    "name_az": "Elektrik enerjisi"
   },
   "legend138": {
     "name_en": "Electricity",
-    "name_az": "Electricity_AZ"
+    "name_az": "Elektrik enerjisi"
   },
   "legend139": {
     "name_en": "Gasoline blend",
-    "name_az": "Gasoline blend_AZ"
+    "name_az": "Benzin qarışığı"
   },
   "legend14": {
     "name_en": "Biogas",
-    "name_az": "Biogas_AZ"
+    "name_az": "Bioqaz"
   },
   "legend140": {
     "name_en": "Gasoline blend",
-    "name_az": "Gasoline blend_AZ"
+    "name_az": "Benzin qarışığı"
   },
   "legend141": {
     "name_en": "Gasoline blend",
-    "name_az": "Gasoline blend_AZ"
+    "name_az": "Benzin qarışığı"
   },
   "legend142": {
     "name_en": "Gasoline blend",
-    "name_az": "Gasoline blend_AZ"
+    "name_az": "Benzin qarışığı"
   },
   "legend143": {
     "name_en": "Bioethanol",
-    "name_az": "Bioethanol_AZ"
+    "name_az": "Bioetanol"
   },
   "legend144": {
     "name_en": "Gasoline",
-    "name_az": "Gasoline_AZ"
+    "name_az": "Benzin"
   },
   "legend145": {
     "name_en": "Hydrogen",
-    "name_az": "Hydrogen_AZ"
+    "name_az": "Hidrogen"
   },
   "legend146": {
     "name_en": "Heavy fuel oil",
-    "name_az": "Heavy fuel oil_AZ"
+    "name_az": "Ağır mazut"
   },
   "legend147": {
     "name_en": "(Bio-)Kerosene blend",
-    "name_az": "(Bio-)Kerosene blend_AZ"
+    "name_az": "(Bio-)Kerosin qarışığı"
   },
   "legend148": {
     "name_en": "(Bio-)Kerosene blend",
-    "name_az": "(Bio-)Kerosene blend_AZ"
+    "name_az": "(Bio-)Kerosin qarışığı"
   },
   "legend149": {
     "name_en": "(Bio-)Kerosene blend",
-    "name_az": "(Bio-)Kerosene blend_AZ"
+    "name_az": "(Bio-)Kerosin qarışığı"
   },
   "legend15": {
     "name_en": "CO2 sink",
-    "name_az": "CO2 sink_AZ"
+    "name_az": "CO2 uducu"
   },
   "legend150": {
     "name_en": "(Bio-)Kerosene blend",
-    "name_az": "(Bio-)Kerosene blend_AZ"
+    "name_az": "(Bio-)Kerosin qarışığı"
   },
   "legend151": {
     "name_en": "(Bio-)Kerosene blend",
-    "name_az": "(Bio-)Kerosene blend_AZ"
+    "name_az": "(Bio-)Kerosin qarışığı"
   },
   "legend152": {
     "name_en": "(Bio-)Kerosene blend",
-    "name_az": "(Bio-)Kerosene blend_AZ"
+    "name_az": "(Bio-)Kerosin qarışığı"
   },
   "legend153": {
     "name_en": "(Bio-)Kerosene blend",
-    "name_az": "(Bio-)Kerosene blend_AZ"
+    "name_az": "(Bio-)Kerosin qarışığı"
   },
   "legend154": {
     "name_en": "Liquefied petroleum gas",
-    "name_az": "Liquefied petroleum gas_AZ"
+    "name_az": "Mayeləşdirilmiş neft qazı"
   },
   "legend155": {
     "name_en": "MGO & Biodiesel blend",
-    "name_az": "MGO & Biodiesel blend_AZ"
+    "name_az": "Gəmi qazoylu və Biodizel qarışığı"
   },
   "legend156": {
     "name_en": "Marine gas oil",
-    "name_az": "Marine gas oil_AZ"
+    "name_az": "Gəmi qazoylu"
   },
   "legend157": {
     "name_en": "Natural gas",
-    "name_az": "Natural gas_AZ"
+    "name_az": "Təbii qaz"
   },
   "legend158": {
     "name_en": "Liq. NGA & SNG blend",
-    "name_az": "Liq. NGA & SNG blend_AZ"
+    "name_az": "Maye təbii qaz və sıxılmış təbii qaz qarışığı"
   },
   "legend159": {
     "name_en": "(Bio-)Oil blend",
-    "name_az": "(Bio-)Oil blend_AZ"
+    "name_az": "(Bio-)Oil blend"
   },
   "legend16": {
     "name_en": "CO2 storage",
-    "name_az": "CO2 storage_AZ"
+    "name_az": "CO2 saxlama"
   },
   "legend160": {
     "name_en": "Synthetic natural gas",
-    "name_az": "Synthetic natural gas_AZ"
+    "name_az": "Sintetik təbii qaz"
   },
   "legend161": {
     "name_en": "Woodchips",
-    "name_az": "Woodchips_AZ"
+    "name_az": "Odun talaşı"
   },
   "legend162": {
     "name_en": "Wind",
-    "name_az": "Wind_AZ"
+    "name_az": "Külək"
   },
   "legend163": {
     "name_en": "Wood pellets",
-    "name_az": "Wood pellets_AZ"
+    "name_az": "Odun yanacaq qranulları"
   },
   "legend164": {
     "name_en": "Waste",
-    "name_az": "Waste_AZ"
+    "name_az": "Tullantı"
   },
   "legend17": {
     "name_en": "Coal",
-    "name_az": "Coal_AZ"
+    "name_az": "Kömür"
   },
   "legend18": {
     "name_en": "Cooling",
-    "name_az": "Cooling_AZ"
+    "name_az": "Soyutma"
   },
   "legend19": {
     "name_en": "Cooling",
-    "name_az": "Cooling_AZ"
+    "name_az": "Soyutma"
   },
   "legend2": {
     "name_en": "Power & Heat",
-    "name_az": "Power & Heat_AZ"
+    "name_az": "Elektrik enerjisi və İstilik"
   },
   "legend20": {
     "name_en": "Cooling",
-    "name_az": "Cooling_AZ"
+    "name_az": "Soyutma"
   },
   "legend21": {
     "name_en": "Cooling",
-    "name_az": "Cooling_AZ"
+    "name_az": "Soyutma"
   },
   "legend22": {
     "name_en": "Electricity",
-    "name_az": "Electricity_AZ"
+    "name_az": "Elektrik enerjisi"
   },
   "legend23": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend24": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend25": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend26": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend27": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend28": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend29": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend3": {
     "name_en": "Industry",
-    "name_az": "Industry_AZ"
+    "name_az": "Sənaye"
   },
   "legend30": {
     "name_en": "Liquefied petroleum gas",
-    "name_az": "Liquefied petroleum gas_AZ"
+    "name_az": "Mayeləşdirilmiş neft qazı"
   },
   "legend31": {
     "name_en": "Natural gas",
-    "name_az": "Natural gas_AZ"
+    "name_az": "Təbii qaz"
   },
   "legend32": {
     "name_en": "Crude oil",
-    "name_az": "Crude oil_AZ"
+    "name_az": "Xam neft"
   },
   "legend33": {
     "name_en": "Corn",
-    "name_az": "Corn_AZ"
+    "name_az": "Qarğıdalı"
   },
   "legend34": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend35": {
     "name_en": "Ethanol",
-    "name_az": "Ethanol_AZ"
+    "name_az": "Etanol"
   },
   "legend36": {
     "name_en": "Deep liter",
-    "name_az": "Deep liter_AZ"
+    "name_az": "Dərin litr"
   },
   "legend37": {
     "name_en": "Biodiesel",
-    "name_az": "Biodiesel_AZ"
+    "name_az": "Biodizel"
   },
   "legend38": {
     "name_en": "Diesel",
-    "name_az": "Diesel_AZ"
+    "name_az": "Dizel"
   },
   "legend39": {
     "name_en": "Electricity",
-    "name_az": "Electricity_AZ"
+    "name_az": "Elektrik enerjisi"
   },
   "legend4": {
     "name_en": "Residential",
-    "name_az": "Residential_AZ"
+    "name_az": "Əhali"
   },
   "legend40": {
     "name_en": "Power & Heat",
-    "name_az": "Power & Heat_AZ"
+    "name_az": "Elektrik enerjisi və İstilik"
   },
   "legend41": {
     "name_en": "Electricity",
-    "name_az": "Electricity_AZ"
+    "name_az": "Elektrik enerjisi"
   },
   "legend42": {
     "name_en": "Electricity",
-    "name_az": "Electricity_AZ"
+    "name_az": "Elektrik enerjisi"
   },
   "legend43": {
     "name_en": "Electricity",
-    "name_az": "Electricity_AZ"
+    "name_az": "Elektrik enerjisi"
   },
   "legend44": {
     "name_en": "Firewood",
-    "name_az": "Firewood_AZ"
+    "name_az": "Odun"
   },
   "legend45": {
     "name_en": "Bioethanol",
-    "name_az": "Bioethanol_AZ"
+    "name_az": "Bioetanol"
   },
   "legend46": {
     "name_en": "Electro Gasoline",
-    "name_az": "Electro Gasoline_AZ"
+    "name_az": "Elektrik-benzin"
   },
   "legend47": {
     "name_en": "Gasoline",
-    "name_az": "Gasoline_AZ"
+    "name_az": "Benzin"
   },
   "legend48": {
     "name_en": "Hydrogen",
-    "name_az": "Hydrogen_AZ"
+    "name_az": "Hidrogen"
   },
   "legend49": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend5": {
     "name_en": "Supply",
-    "name_az": "Supply_AZ"
+    "name_az": "Təchizat"
   },
   "legend50": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend51": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend52": {
     "name_en": "Heavy fuel oil",
-    "name_az": "Heavy fuel oil_AZ"
+    "name_az": "Ağır mazut"
   },
   "legend53": {
     "name_en": "Hydro",
-    "name_az": "Hydro_AZ"
+    "name_az": "Su"
   },
   "legend54": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend55": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend56": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend57": {
     "name_en": "Biogas",
-    "name_az": "Biogas_AZ"
+    "name_az": "Bioqaz"
   },
   "legend58": {
     "name_en": "Coal",
-    "name_az": "Coal_AZ"
+    "name_az": "Kömür"
   },
   "legend59": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend6": {
     "name_en": "Transport",
-    "name_az": "Transport_AZ"
+    "name_az": "Nəqliyyat"
   },
   "legend60": {
     "name_en": "Diesel",
-    "name_az": "Diesel_AZ"
+    "name_az": "Dizel"
   },
   "legend61": {
     "name_en": "Electricity",
-    "name_az": "Electricity_AZ"
+    "name_az": "Elektrik enerjisi"
   },
   "legend62": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend63": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend64": {
     "name_en": "Hydrogen",
-    "name_az": "Hydrogen_AZ"
+    "name_az": "Hidrogen"
   },
   "legend65": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend66": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend67": {
     "name_en": "Heavy fuel oil",
-    "name_az": "Heavy fuel oil_AZ"
+    "name_az": "Ağır mazut"
   },
   "legend68": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend69": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend7": {
     "name_en": "Biogas",
-    "name_az": "Biogas_AZ"
+    "name_az": "Bioqaz"
   },
   "legend70": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend71": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend72": {
     "name_en": "Natural gas",
-    "name_az": "Natural gas_AZ"
+    "name_az": "Təbii qaz"
   },
   "legend73": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend74": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend75": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend76": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend77": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend78": {
     "name_en": "Woodchips",
-    "name_az": "Woodchips_AZ"
+    "name_az": "Odun talaşı"
   },
   "legend79": {
     "name_en": "Waste",
-    "name_az": "Waste_AZ"
+    "name_az": "Tullantı"
   },
   "legend8": {
     "name_en": "Diesel",
-    "name_az": "Diesel_AZ"
+    "name_az": "Dizel"
   },
   "legend80": {
     "name_en": "Heat",
-    "name_az": "Heat_AZ"
+    "name_az": "İstilik"
   },
   "legend81": {
     "name_en": "Kerosene",
-    "name_az": "Kerosene_AZ"
+    "name_az": "Kerosin"
   },
   "legend82": {
     "name_en": "Bio kerosene",
-    "name_az": "Bio kerosene_AZ"
+    "name_az": "Bio kerosin"
   },
   "legend83": {
     "name_en": "Electro kerosene",
-    "name_az": "Electro kerosene_AZ"
+    "name_az": "Electro kerosene"
   },
   "legend84": {
     "name_en": "Liquefied petroleum gas",
-    "name_az": "Liquefied petroleum gas_AZ"
+    "name_az": "Mayeləşdirilmiş neft qazı"
   },
   "legend85": {
     "name_en": "Naphtha",
-    "name_az": "Naphtha_AZ"
+    "name_az": "Nafta"
   },
   "legend86": {
     "name_en": "Crude oil",
-    "name_az": "Crude oil_AZ"
+    "name_az": "Xam neft"
   },
   "legend87": {
     "name_en": "Marine gasoline",
-    "name_az": "Marine gasoline_AZ"
+    "name_az": "Gəmi benzini"
   },
   "legend88": {
     "name_en": "Natural gas",
-    "name_az": "Natural gas_AZ"
+    "name_az": "Təbii qaz"
   },
   "legend89": {
     "name_en": "Manure",
-    "name_az": "Manure_AZ"
+    "name_az": "Peyin"
   },
   "legend9": {
     "name_en": "Electricity",
-    "name_az": "Electricity_AZ"
+    "name_az": "Elektrik enerjisi"
   },
   "legend90": {
     "name_en": "Bio Methanol",
-    "name_az": "Bio Methanol_AZ"
+    "name_az": "Bio metanol"
   },
   "legend91": {
     "name_en": "Bio Methanol",
-    "name_az": "Bio Methanol_AZ"
+    "name_az": "Bio metanol"
   },
   "legend92": {
     "name_en": "Natural gas",
-    "name_az": "Natural gas_AZ"
+    "name_az": "Təbii qaz"
   },
   "legend93": {
     "name_en": "Natural gas",
-    "name_az": "Natural gas_AZ"
+    "name_az": "Təbii qaz"
   },
   "legend94": {
     "name_en": "Other",
-    "name_az": "Other_AZ"
+    "name_az": "Digər"
   },
   "legend95": {
     "name_en": "Biomass",
-    "name_az": "Biomass_AZ"
+    "name_az": "Biokütlə"
   },
   "legend96": {
     "name_en": "Hydro",
-    "name_az": "Hydro_AZ"
+    "name_az": "Su"
   },
   "legend97": {
     "name_en": "Natural gas",
-    "name_az": "Natural gas_AZ"
+    "name_az": "Təbii qaz"
   },
   "legend98": {
     "name_en": "Other",
-    "name_az": "Other_AZ"
+    "name_az": "Digər"
   },
   "legend99": {
     "name_en": "Solar",
-    "name_az": "Solar_AZ"
+    "name_az": "Günəş"
   }
 }
--- src/translations/tabs.js
+++ src/translations/tabs.js
@@ -1,22 +1,22 @@
 export default {
   "tab1": {
     "name_en": "Main Results",
-    "name_az": "Main Results AZ"
+    "name_az": "Əsas nəticələr"
   },
   "tab2": {
     "name_en": "Upstream",
-    "name_az": "Upstream AZ"
+    "name_az": "Neft-qaz kəşfiyyatı və hasilatı"
   },
   "tab3": {
     "name_en": "Power & Heat",
-    "name_az": "Power & Heat_AZ"
+    "name_az": "Elektrik enerjisi və İstilik"
   },
   "tab4": {
     "name_en": "Transport",
-    "name_az": "Transport_AZ"
+    "name_az": "Nəqliyyat"
   },
   "tab5": {
     "name_en": "Other End-Use",
-    "name_az": "Other End-Use AZ"
+    "name_az": "Other End-Use"
   }
 }
