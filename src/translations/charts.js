a0dc4dd7c28f8c45334c3d2bd534cc6a66b8e4a3
tilseb <till@energymodellinglab.com>
2021-10-19 10:44:47 +0200

    Add charts.js with type and units.js Add units.js defining alternative units with conversion factors

--- src/translations/charts.js
+++ src/translations/charts.js
@@ -1,81 +1,121 @@
 export default {
   "chart-titles.tab1.chart1": {
+    "type": "stackedBar",
+    "unit": "PJ",
     "name_en": "Primary Energy Supply",
     "name_az": "Primary Energy Supply AZ"
   },
   "chart-titles.tab1.chart2": {
+    "type": "stackedBar",
+    "unit": "PJ",
     "name_en": "Final Energy Consumption",
     "name_az": "Final Energy Consumption AZ"
   },
   "chart-titles.tab1.chart3": {
+    "type": "line",
+    "unit": "kt",
     "name_en": "Negative CO2 Emissions",
     "name_az": "Negative CO2 Emissions AZ"
   },
   "chart-titles.tab1.chart4": {
+    "type": "line",
+    "unit": "kt",
     "name_en": "Positive CO2 Emissions",
     "name_az": "Positive CO2 Emissions AZ"
   },
   "chart-titles.tab1.chart5": {
+    "type": "stackedBar",
+    "unit": "PJ",
     "name_en": "Energy Exported",
     "name_az": "Energy Exported AZ"
   },
   "chart-titles.tab1.chart6": {
+    "type": "stackedBar",
+    "unit": "PJ",
     "name_en": "Energy Imported",
     "name_az": "Energy Imported AZ"
   },
   "chart-titles.tab2.chart1": {
+    "type": "stackedBar",
+    "unit": "PJ",
     "name_en": "Fossil Refineries Input",
     "name_az": "Fossil Refineries Input AZ"
   },
   "chart-titles.tab2.chart2": {
+    "type": "stackedBar",
+    "unit": "PJ",
     "name_en": "Fossil Refineries Output",
     "name_az": "Fossil Refineries Output AZ"
   },
   "chart-titles.tab3.chart1": {
+    "type": "stackedBar",
+    "unit": "MW",
     "name_en": "Power Capacity",
     "name_az": "Power Capacity AZ"
   },
   "chart-titles.tab3.chart2": {
+    "type": "stackedBar",
+    "unit": "PJ",
     "name_en": "Power Production",
     "name_az": "Power Production AZ"
   },
   "chart-titles.tab4.chart1": {
+    "type": "stackedBar",
+    "unit": "PJ",
     "name_en": "Transport Fuel Consumption",
     "name_az": "Transport Fuel Consumption AZ"
   },
   "chart-titles.tab4.chart2": {
+    "type": "stackedBar",
+    "unit": "kt CO2",
     "name_en": "Transport CO2 Emissions",
     "name_az": "Transport CO2 Emissions AZ"
   },
   "chart-titles.tab5.chart1": {
+    "type": "stackedBar",
+    "unit": "PJ",
     "name_en": "Agriculture Fuel Consumption",
     "name_az": "Agriculture Fuel Consumption AZ"
   },
   "chart-titles.tab5.chart2": {
+    "type": "stackedBar",
+    "unit": "PJ",
     "name_en": "Commercial Fuel Consumption",
     "name_az": "Commercial Fuel Consumption AZ"
   },
   "chart-titles.tab5.chart3": {
+    "type": "stackedBar",
+    "unit": "PJ",
     "name_en": "Industry Fuel Consumption",
     "name_az": "Industry Fuel Consumption AZ"
   },
   "chart-titles.tab5.chart4": {
+    "type": "stackedBar",
+    "unit": "PJ",
     "name_en": "Residential Fuel Consumption",
     "name_az": "Residential Fuel Consumption AZ"
   },
   "chart-titles.tab5.chart5": {
+    "type": "stackedBar",
+    "unit": "kt CO2",
     "name_en": "Agriculture CO2 Emissions",
     "name_az": "Agriculture CO2 Emissions AZ"
   },
   "chart-titles.tab5.chart6": {
+    "type": "stackedBar",
+    "unit": "kt CO2",
     "name_en": "Commercial CO2 Emissions",
     "name_az": "Commercial CO2 Emissions AZ"
   },
   "chart-titles.tab5.chart7": {
+    "type": "stackedBar",
+    "unit": "kt CO2",
     "name_en": "Industry CO2 Emissions",
     "name_az": "Industry CO2 Emissions AZ"
   },
   "chart-titles.tab5.chart8": {
+    "type": "stackedBar",
+    "unit": "kt CO2",
     "name_en": "Residential CO2 Emissions",
     "name_az": "Residential CO2 Emissions AZ"
   }
