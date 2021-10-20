464a2cc9f9c8937aef6d4f8974d4e2b49cfadceb
amaraiw <andrea@energymodellinglab.com>
2021-10-20 16:27:36 +0200

    Add tab6 and chart6

---
+++ src/data/chartstab6.js
@@ -0,0 +1,8 @@
+export default 
+ 	 [ 
+		"chart-titles.tab6.chart1",
+		"chart-titles.tab6.chart2",
+		"chart-titles.tab6.chart3",
+		"chart-titles.tab6.chart4",
+		"chart-titles.tab6.chart5",
+	 ]
