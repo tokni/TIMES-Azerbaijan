28c2737d17be0f226455eb6ad3ca68ee97d38d68
tilseb <till@energymodellinglab.com>
2021-10-25 14:30:38 +0200

    Switch to standard unit

--- src/translations/units.js
+++ src/translations/units.js
@@ -1,39 +1,10 @@
 export default {
-  "MW": {
-    "MW": 1.0,
-    "GW": 0.001
+  "Euro / MWh": {
+    "EURO / toe": 0.0859845,
+    "Euro / MWh": 1.0
   },
-  "Mill.pkm": {
-    "Mill.pkm": 1.0,
-    "Bill.pkm": 0.001
-  },
-  "Mill.tkm": {
-    "Mill.tkm": 1.0,
-    "Bill.tkm": 0.001
-  },
-  "Mio. Euro 2020": {
-    "Mio. Euro 2020": 1.0,
-    "Bn. Euro 2020": 0.001
-  },
-  "Mio. Euro 2020 / PJ": {
-    "Mio. Euro 2020 / PJ": 1.0,
-    "Euro 2020 / Mwh": 3.6
-  },
-  "Mio. Euro 2020 / kt CO2": {
-    "Mio. Euro 2020 / kt CO2": 1.0,
-    "Euro 2020 / tCO2": 1000.0
-  },
-  "PJ": {
-    "PJ": 1.0,
-    "Mtoe": 0.0238845897,
-    "TWh": 0.2777777778
-  },
-  "kt CO2": {
-    "kt CO2": 1.0,
-    "Mt CO2": 0.001
-  },
-  "kvehicles": {
-    "kvehicles": 1.0,
-    "Mill.vehicles": 0.001
+  "TWh": {
+    "Mtoe": 0.0859845,
+    "TWh": 1.0
   }
 }
