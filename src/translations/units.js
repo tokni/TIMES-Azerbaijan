2ff84a0a719bdba68ad70ac6444f0e474eaaf34a
Seregfaroth <bl@tokni.com>
2021-10-25 20:33:13 +0100

    quick unit fix

--- src/translations/units.js
+++ src/translations/units.js
@@ -1,10 +1,10 @@
 export default {
   "Euro / MWh": {
+    "Euro / MWh": 1.0,
     "EURO / toe": 0.0859845,
-    "Euro / MWh": 1.0
   },
   "TWh": {
+    "TWh": 1.0,
     "Mtoe": 0.0859845,
-    "TWh": 1.0
   }
 }
