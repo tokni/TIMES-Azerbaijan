638ad1d1d303d0308ecef085e9c8b2db89273882
tilseb <till@energymodellinglab.com>
2021-10-25 11:55:18 +0200

    Add more translation

--- src/translations/charts.js
+++ src/translations/charts.js
@@ -3,55 +3,55 @@ export default {
     "type": "stackedBar",
     "unit": "PJ",
     "name_en": "Primary Energy Supply by Energy Source",
-    "name_az": "not_defined"
+    "name_az": "Enerji mənbəyinə görə son enerji təchizatı"
   },
   "chart-titles.tab1.chart2": {
     "type": "stackedBar",
     "unit": "PJ",
     "name_en": "Final Energy Consumption by Energy Source",
-    "name_az": "not_defined"
+    "name_az": "Enerji mənbəyinə görə son enerji istehlakı"
   },
   "chart-titles.tab1.chart3": {
     "type": "stackedBar",
     "unit": "kt CO2",
     "name_en": "CO2 Emissions",
-    "name_az": "not_defined"
+    "name_az": "CO2 tullantıları"
   },
   "chart-titles.tab1.chart4": {
     "type": "stackedBar",
     "unit": "PJ",
     "name_en": "Export of Energy",
-    "name_az": "not_defined"
+    "name_az": "Enerji ixracı"
   },
   "chart-titles.tab1.chart5": {
     "type": "stackedBar",
     "unit": "PJ",
     "name_en": "Import of Energy",
-    "name_az": "not_defined"
+    "name_az": "Enerji idxalı"
   },
   "chart-titles.tab1.chart6": {
     "type": "stackedBar",
     "unit": "Mio. Euro 2020 / kt CO2",
     "name_en": "Marginal Prices – CO2",
-    "name_az": "not_defined"
+    "name_az": "Marjinal Qiymətlər – CO2"
   },
   "chart-titles.tab1.chart7": {
     "type": "stackedBar",
     "unit": "Mio. Euro 2020",
     "name_en": "Exports Cost",
-    "name_az": "not_defined"
+    "name_az": "İxrac Xərcləri"
   },
   "chart-titles.tab1.chart8": {
     "type": "stackedBar",
     "unit": "Mio. Euro 2020",
     "name_en": "Imports Cost",
-    "name_az": "not_defined"
+    "name_az": "İdxal Xərcləri"
   },
   "chart-titles.tab1.chart9": {
     "type": "stackedBar",
     "unit": "Mio. Euro 2020",
     "name_en": "System Cost",
-    "name_az": "not_defined"
+    "name_az": "Sistemin xərcləri"
   },
   "chart-titles.tab2.chart1": {
     "type": "stackedBar",
@@ -69,31 +69,31 @@ export default {
     "type": "stackedBar",
     "unit": "PJ",
     "name_en": "RE Refineries Input",
-    "name_az": "not_defined"
+    "name_az": "BOE emalı zavodlarının payı"
   },
   "chart-titles.tab2.chart4": {
     "type": "stackedBar",
     "unit": "PJ",
     "name_en": "RE Refineries Output",
-    "name_az": "not_defined"
+    "name_az": "BOE emalı zavodlarının generasiya gücü"
   },
   "chart-titles.tab2.chart5": {
     "type": "stackedBar",
     "unit": "kt CO2",
     "name_en": "Upstream Sector CO2 Emissions",
-    "name_az": "not_defined"
+    "name_az": "Neft-qaz emalı sektorunda CO2 tullantıları"
   },
   "chart-titles.tab2.chart6": {
     "type": "stackedBar",
     "unit": "PJ",
     "name_en": "Primary Biomass Supply",
-    "name_az": "not_defined"
+    "name_az": "İlkin Biokütlə Təchizatı"
   },
   "chart-titles.tab2.chart7": {
     "type": "stackedBar",
     "unit": "Mio. Euro 2020",
     "name_en": "Upstream System Cost",
-    "name_az": "not_defined"
+    "name_az": "Neft-qaz emalı sisteminin xərcləri"
   },
   "chart-titles.tab3.chart1": {
     "type": "stackedBar",
@@ -104,32 +104,38 @@ export default {
   "chart-titles.tab3.chart10": {
     "type": "stackedBar",
     "unit": "Mio. Euro 2020 / PJ",
-    "name_en": "Marginal Prices – District Heat Urban Suburban Rural",
-    "name_az": "not_defined"
+    "name_en": "Marginal Prices – District Heat",
+    "name_az": "Marjinal qiymətlər - mərkəzi istilik"
   },
   "chart-titles.tab3.chart11": {
     "type": "stackedBar",
+    "unit": "Mio. Euro 2020 / PJ",
+    "name_en": "Marginal Prices – District Heat Urban Suburban Rural",
+    "name_az": "Marjinal Qiymətlər – Mərkəzləşdirilmiş istilik - şəhər, şəhərətrafı və kənd əraziləri"
+  },
+  "chart-titles.tab3.chart12": {
+    "type": "stackedBar",
     "unit": "Mio. Euro 2020",
     "name_en": "Power and Heat System Cost",
-    "name_az": "not_defined"
+    "name_az": "Elektrik enerjisi və istilik sistemi xərcləri"
   },
   "chart-titles.tab3.chart2": {
     "type": "stackedBar",
     "unit": "PJ",
     "name_en": "Power Production per Energy Source",
-    "name_az": "not_defined"
+    "name_az": "Enerji Mənbəyinə görə elektrik enerjisi istehsalı"
   },
   "chart-titles.tab3.chart3": {
     "type": "stackedBar",
     "unit": "kt CO2",
     "name_en": "Power and Heat CO2 Emissions",
-    "name_az": "not_defined"
+    "name_az": "Elektrik enerjisi və İstilik CO2 tullantıları"
   },
   "chart-titles.tab3.chart4": {
     "type": "stackedBar",
     "unit": "PJ",
     "name_en": "Power and Heat Fuel Consumption",
-    "name_az": "not_defined"
+    "name_az": "Elektrik enerjisi və İstilik istehsalında yanacaq sərfiyyatı"
   },
   "chart-titles.tab3.chart5": {
     "type": "stackedBar",
@@ -140,26 +146,26 @@ export default {
   "chart-titles.tab3.chart6": {
     "type": "stackedBar",
     "unit": "PJ",
-    "name_en": "District Heat Production",
-    "name_az": "not_defined"
+    "name_en": "International Power Trade",
+    "name_az": "International Power Trade"
   },
   "chart-titles.tab3.chart7": {
+    "type": "stackedBar",
+    "unit": "PJ",
+    "name_en": "District Heat Production",
+    "name_az": "Mərkəzləşdirilmiş İstilik İstehsalı"
+  },
+  "chart-titles.tab3.chart8": {
     "type": "stackedBar",
     "unit": "PJ",
     "name_en": "Power Consumption by Sector",
     "name_az": "Sektorlar üzrə elektrik enerjisi sərfi"
   },
-  "chart-titles.tab3.chart8": {
+  "chart-titles.tab3.chart9": {
     "type": "stackedBar",
     "unit": "Mio. Euro 2020 / PJ",
     "name_en": "Marginal Prices – Electricity",
     "name_az": "Marjinal qiymətlər - elektrik enerjisi"
-  },
-  "chart-titles.tab3.chart9": {
-    "type": "stackedBar",
-    "unit": "Mio. Euro 2020 / PJ",
-    "name_en": "Marginal Prices – District Heat",
-    "name_az": "Marjinal qiymətlər - mərkəzi istilik"
   },
   "chart-titles.tab4.chart1": {
     "type": "stackedBar",
@@ -177,19 +183,19 @@ export default {
     "type": "stackedBar",
     "unit": "PJ",
     "name_en": "Glass and Concrete Fuel Consumption",
-    "name_az": "not_defined"
+    "name_az": "Şüşə və beton istehsalında yanacaq sərfiyyatı"
   },
   "chart-titles.tab4.chart4": {
     "type": "stackedBar",
     "unit": "PJ",
     "name_en": "Iron Steel and Aluminium Fuel Consumption",
-    "name_az": "not_defined"
+    "name_az": "Dəmir polad və alüminium istehsalında yanacaq sərfiyyatı"
   },
   "chart-titles.tab4.chart5": {
     "type": "stackedBar",
     "unit": "PJ",
     "name_en": "Other Industry Fuel Consumption",
-    "name_az": "not_defined"
+    "name_az": "Digər sənaye sahələrində yanacaq sərfiyyatı"
   },
   "chart-titles.tab4.chart6": {
     "type": "stackedBar",
@@ -201,13 +207,13 @@ export default {
     "type": "stackedBar",
     "unit": "PJ",
     "name_en": "Industry Energy Service Demand",
-    "name_az": "not_defined"
+    "name_az": "Sənayenin enerji xidmətinə tələbatı"
   },
   "chart-titles.tab4.chart8": {
     "type": "stackedBar",
     "unit": "Mio. Euro 2020",
     "name_en": "Industry System Cost",
-    "name_az": "not_defined"
+    "name_az": "Sənayedə sistem xərcləri"
   },
   "chart-titles.tab5.chart1": {
     "type": "stackedBar",
@@ -225,25 +231,25 @@ export default {
     "type": "stackedBar",
     "unit": "Mill.tkm",
     "name_en": "Freight Transport Demand",
-    "name_az": "not_defined"
+    "name_az": "Yük daşımalarına tələbat"
   },
   "chart-titles.tab5.chart4": {
     "type": "stackedBar",
     "unit": "Mill.pkm",
     "name_en": "Passenger Transport Demand",
-    "name_az": "not_defined"
+    "name_az": "Sərnişin daşımalarına tələbat"
   },
   "chart-titles.tab5.chart5": {
     "type": "stackedBar",
     "unit": "PJ",
     "name_en": "Transport Freight Fuel Consumption",
-    "name_az": "not_defined"
+    "name_az": "Yükdaşıma nəqliyyatında yanacaq sərfiyyatı"
   },
   "chart-titles.tab5.chart6": {
     "type": "stackedBar",
     "unit": "PJ",
     "name_en": "Transport Passenger Fuel Consumption",
-    "name_az": "not_defined"
+    "name_az": "Sərnişin daşıma nəqliyyatında yanacaq sərfiyyatı"
   },
   "chart-titles.tab5.chart7": {
     "type": "stackedBar",
@@ -255,7 +261,7 @@ export default {
     "type": "stackedBar",
     "unit": "Mio. Euro 2020",
     "name_en": "Transport System Cost",
-    "name_az": "not_defined"
+    "name_az": "Nəqliyyat sisteminin xərcləri"
   },
   "chart-titles.tab6.chart1": {
     "type": "stackedBar",
@@ -285,6 +291,6 @@ export default {
     "type": "stackedBar",
     "unit": "Mio. Euro 2020",
     "name_en": "Other Sectors System Cost",
-    "name_az": "not_defined"
+    "name_az": "Digər sektorlar üzrə sistem xərcləri"
   }
 }
