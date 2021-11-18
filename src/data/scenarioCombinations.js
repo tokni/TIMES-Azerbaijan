97ae78557323bb06abd3ee60cb9d220055f1949f
amaraiw <andrea@energymodellinglab.com>
2021-11-18 13:38:26 +0100

    scenarioCombinations.js for results 18112021

--- src/data/scenarioCombinations.js
+++ src/data/scenarioCombinations.js
@@ -11,107 +11,7 @@ export default {
         "name_az": "Bazis",
         "short_description_az": "Bazis",
         "ultra_short_description_az": "B",
-        "desc_az": "Vergilər/subsidiyalar və planlaşdırılan yeni enerji gücü timsalında artıq həyata keçirilən siyasətlərin davam etdirilməsi. Enerji daşıyıcılarının ticarətinin və bütün sektorlarda yeni gücə investisiya qoyuluşunun rəqabətli bazarda baş verəcəyi fərziyyəsi ",
-        "opt0": false,
-        "opt1": false
-      },
-      {
-        "id": "scenario0_copy",
-        "name_en": "Reference",
-        "short_description_en": "Reference",
-        "ultra_short_description_en": "R",
-        "desc_en": "Continuing already implemented policies as taxes/subsidies and planned new power capacity. It is assumed that trade of energy carriers and investment in new capacity in all sectors will happen on a competing market.",
-        "id_noOptions": "scenario0_copy",
-        "name_az": "Bazis",
-        "short_description_az": "Bazis",
-        "ultra_short_description_az": "B",
-        "desc_az": "Vergilər/subsidiyalar və planlaşdırılan yeni enerji gücü timsalında artıq həyata keçirilən siyasətlərin davam etdirilməsi. Enerji daşıyıcılarının ticarətinin və bütün sektorlarda yeni gücə investisiya qoyuluşunun rəqabətli bazarda baş verəcəyi fərziyyəsi ",
-        "opt0": false,
-        "opt1": false
-      },
-      {
-        "id": "scenario0_copy_opt0",
-        "name_en": "Reference_Bio",
-        "short_description_en": "Reference_Bio",
-        "ultra_short_description_en": "RBIO",
-        "desc_en": "Bio",
-        "id_noOptions": "scenario0_copy",
-        "name_az": "Reference_Bio",
-        "short_description_az": "Reference_Bio",
-        "ultra_short_description_az": "RBIO",
-        "desc_az": "Bio",
-        "opt0": true,
-        "opt1": false
-      },
-      {
-        "id": "scenario0_copy_opt0_opt1",
-        "name_en": "Reference_Bio_CCS",
-        "short_description_en": "Reference_Bio_CCS",
-        "ultra_short_description_en": "RBIOCCS",
-        "desc_en": "Bio and CCS",
-        "id_noOptions": "scenario0_copy",
-        "name_az": "Reference_Bio_CCS",
-        "short_description_az": "Reference_Bio_CCS",
-        "ultra_short_description_az": "RBIOCCS",
-        "desc_az": "Bio and CCS",
-        "opt0": true,
-        "opt1": true
-      },
-      {
-        "id": "scenario0_copy_opt1",
-        "name_en": "Reference_CCS",
-        "short_description_en": "Reference_CCS",
-        "ultra_short_description_en": "RCCS",
-        "desc_en": "CCS",
-        "id_noOptions": "scenario0_copy",
-        "name_az": "Reference_CCS",
-        "short_description_az": "Reference_CCS",
-        "ultra_short_description_az": "RCCS",
-        "desc_az": "CCS",
-        "opt0": false,
-        "opt1": true
-      },
-      {
-        "id": "scenario0_opt0",
-        "name_en": "Reference_Bio",
-        "short_description_en": "Reference_Bio",
-        "ultra_short_description_en": "RBIO",
-        "desc_en": "Bio",
-        "id_noOptions": "scenario0",
-        "name_az": "Reference_Bio",
-        "short_description_az": "Reference_Bio",
-        "ultra_short_description_az": "RBIO",
-        "desc_az": "Bio",
-        "opt0": true,
-        "opt1": false
-      },
-      {
-        "id": "scenario0_opt0_opt1",
-        "name_en": "Reference_Bio_CCS",
-        "short_description_en": "Reference_Bio_CCS",
-        "ultra_short_description_en": "RBIOCCS",
-        "desc_en": "Bio and CCS",
-        "id_noOptions": "scenario0",
-        "name_az": "Reference_Bio_CCS",
-        "short_description_az": "Reference_Bio_CCS",
-        "ultra_short_description_az": "RBIOCCS",
-        "desc_az": "Bio and CCS",
-        "opt0": true,
-        "opt1": true
-      },
-      {
-        "id": "scenario0_opt1",
-        "name_en": "Reference_CCS",
-        "short_description_en": "Reference_CCS",
-        "ultra_short_description_en": "RCCS",
-        "desc_en": "CCS",
-        "id_noOptions": "scenario0",
-        "name_az": "Reference_CCS",
-        "short_description_az": "Reference_CCS",
-        "ultra_short_description_az": "RCCS",
-        "desc_az": "CCS",
-        "opt0": false,
-        "opt1": true
+        "desc_az": "Vergilər/subsidiyalar və planlaşdırılan yeni enerji gücü timsalında artıq həyata keçirilən siyasətlərin davam etdirilməsi. Enerji daşıyıcılarının ticarətinin və bütün sektorlarda yeni gücə investisiya qoyuluşunun rəqabətli bazarda baş verəcəyi fərziyyəsi "
       },
       {
         "id": "scenario1",
@@ -123,51 +23,7 @@ export default {
         "name_az": "KarbonNeytrallığı",
         "short_description_az": "KarbonNeytrallığı",
         "ultra_short_description_az": "K",
-        "desc_az": "Baza ssenarisi ilə eyni fərziyyələr irəli sürülür, lakin Azərbaycanın bütöv energetika sistemindən CO2 tullantıları üzrə 2050-ci ilə qədər sıfır tullantı məhdudiyyəti qoyulur.",
-        "opt0": false,
-        "opt1": false
-      },
-      {
-        "id": "scenario1_copy",
-        "name_en": "CarbonNeutral",
-        "short_description_en": "CarbonNeutral",
-        "ultra_short_description_en": "C",
-        "desc_en": "Same assumptions as in Base, but with a constraint on the CO2-emissions from the whole energy system of Azerbaijan securing net-zero emissions by 2050.",
-        "id_noOptions": "scenario1_copy",
-        "name_az": "KarbonNeytrallığı",
-        "short_description_az": "KarbonNeytrallığı",
-        "ultra_short_description_az": "K",
-        "desc_az": "Baza ssenarisi ilə eyni fərziyyələr irəli sürülür, lakin Azərbaycanın bütöv energetika sistemindən CO2 tullantıları üzrə 2050-ci ilə qədər sıfır tullantı məhdudiyyəti qoyulur.",
-        "opt0": false,
-        "opt1": false
-      },
-      {
-        "id": "scenario1_copy_opt0",
-        "name_en": "CarbonNeutral_Bio",
-        "short_description_en": "CarbonNeutral_Bio",
-        "ultra_short_description_en": "CBIO",
-        "desc_en": "Bio",
-        "id_noOptions": "scenario1_copy",
-        "name_az": "CarbonNeutral_Bio",
-        "short_description_az": "CarbonNeutral_Bio",
-        "ultra_short_description_az": "CBIO",
-        "desc_az": "Bio",
-        "opt0": true,
-        "opt1": false
-      },
-      {
-        "id": "scenario1_opt0",
-        "name_en": "CarbonNeutral_Bio",
-        "short_description_en": "CarbonNeutral_Bio",
-        "ultra_short_description_en": "CBIO",
-        "desc_en": "Bio",
-        "id_noOptions": "scenario1",
-        "name_az": "CarbonNeutral_Bio",
-        "short_description_az": "CarbonNeutral_Bio",
-        "ultra_short_description_az": "CBIO",
-        "desc_az": "Bio",
-        "opt0": true,
-        "opt1": false
+        "desc_az": "Baza ssenarisi ilə eyni fərziyyələr irəli sürülür, lakin Azərbaycanın bütöv energetika sistemindən CO2 tullantıları üzrə 2050-ci ilə qədər sıfır tullantı məhdudiyyəti qoyulur."
       },
       {
         "id": "scenario2",
@@ -179,50 +35,33 @@ export default {
         "name_az": "CO2Vergisi",
         "short_description_az": "CO2Vergisi",
         "ultra_short_description_az": "V",
-        "desc_az": "Karbon vergisi ssenarisi",
-        "opt0": false,
-        "opt1": false
+        "desc_az": "Karbon vergisi ssenarisi"
       },
       {
         "id": "scenario3",
-        "name_en": "ClimateTarget",
-        "short_description_en": "ClimateTarget",
-        "ultra_short_description_en": "TRG",
-        "desc_en": "Climate target scenario",
+        "name_en": "CCCS",
+        "short_description_en": "CCCS",
+        "ultra_short_description_en": "CCCS",
+        "desc_en": "Climate Change Compliance Scenario",
         "id_noOptions": "scenario3",
-        "name_az": "IqlimHədəf",
-        "short_description_az": "IqlimHədəf",
-        "ultra_short_description_az": "I",
-        "desc_az": "Iqlim hədəfi ssenarisi",
-        "opt0": false,
-        "opt1": false
+        "name_az": "CCCS",
+        "short_description_az": "CCCS",
+        "ultra_short_description_az": "CCCS",
+        "desc_az": "Climate Change Compliance Scenario"
+      },
+      {
+        "id": "scenario4",
+        "name_en": "CCHD",
+        "short_description_en": "CCHD",
+        "ultra_short_description_en": "CCHD",
+        "desc_en": "Climate Change and High Development",
+        "id_noOptions": "scenario4",
+        "name_az": "CCHD",
+        "short_description_az": "CCHD",
+        "ultra_short_description_az": "CCHD",
+        "desc_az": "Climate Change and High Development"
       }
     ],
-    "optionsAvailable": {
-      "scenario0": {
-        "opt0": true,
-        "opt1": true
-      },
-      "scenario0_copy": {
-        "opt0": true,
-        "opt1": true
-      },
-      "scenario1": {
-        "opt0": true,
-        "opt1": false
-      },
-      "scenario1_copy": {
-        "opt0": true,
-        "opt1": false
-      },
-      "scenario2": {
-        "opt0": false,
-        "opt1": false
-      },
-      "scenario3": {
-        "opt0": false,
-        "opt1": false
-      }
-    }
+    "optionsAvailable": {}
   }
 }
